1f9232684372f53ceb2950d3b5d366e8f82bed81
Nikolay Viguro <nv@ph-systems.ru>
2014-10-09 13:17:08 +0400

    Calendar template

---
+++ app/controllers/Calendar.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import models.Device;
+import models.Log;
+import models.User;
+import play.mvc.Before;
+import play.mvc.Controller;
+import play.mvc.With;
+
+import java.util.List;
+
+@With(Secure.class)
+public class Calendar extends Controller {
+
+    @Before
+    static void setConnectedUser() {
+        if(Security.isConnected()) {
+            User user = User.find("byEmail", Security.connected()).first();
+            renderArgs.put("user", user.name);
+        }
+    }
+
+    public static void index() {
+        render();
+    }
+}
