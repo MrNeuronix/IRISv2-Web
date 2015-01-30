4c9f6d7395172380036eb775e7f3ab308eed11f4
Nikolay Viguro <nv@ph-systems.ru>
2015-01-30 11:06:21 +0300

    Start screen for terminal

--- public/terminal/js/websocket.js
+++ public/terminal/js/websocket.js
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2010 Animesh Kumar  (https://github.com/anismiles)
  * Copyright (c) 2010 Strumsoft  (https://strumsoft.com)
+ * Copyright (c) 2015 Nikolay Viguro  (IRISv2)
  *  
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -26,70 +27,77 @@
  */
 (function() {
 	
-	// window object
-	var global = window;
-	
-	// WebSocket Object. All listener methods are cleaned up!
-	var WebSocket = global.WebSocket = function(url) {
-		// get a new websocket object from factory (check com.strumsoft.websocket.WebSocketFactory.java)
-		this.socket = WebSocketFactory.getInstance(url);
-		// store in registry
-		if(this.socket) {
-			WebSocket.store[this.socket.getId()] = this;
-		} else {
-			throw new Error('Websocket instantiation failed! Address might be wrong.');
+	if ("WebSocket" in window)
+		{
+			// WebSocket supported in browser
+			// Nothing to do
 		}
-	};
-	
-	// storage to hold websocket object for later invokation of event methods
-	WebSocket.store = {};
-	
-	// static event methods to call event methods on target websocket objects
-	WebSocket.onmessage = function (evt) {
-		WebSocket.store[evt._target]['onmessage'].call(global, evt);
-	}	
-	
-	WebSocket.onopen = function (evt) {
-		WebSocket.store[evt._target]['onopen'].call(global, evt);
-	}
-	
-	WebSocket.onclose = function (evt) {
-		WebSocket.store[evt._target]['onclose'].call(global, evt);
-	}
-	
-	WebSocket.onerror = function (evt) {
-		WebSocket.store[evt._target]['onerror'].call(global, evt);
-	}
+		else
+		{
+			var global = window;
 
-	// instance event methods
-	WebSocket.prototype.send = function(data) {
-		this.socket.send(data);
-	}
+			// WebSocket Object. All listener methods are cleaned up!
+			var WebSocket = global.WebSocket = function(url) {
+				// get a new websocket object from factory (check com.strumsoft.websocket.WebSocketFactory.java)
+				this.socket = WebSocketFactory.getInstance(url);
+				// store in registry
+				if(this.socket) {
+					WebSocket.store[this.socket.getId()] = this;
+				} else {
+					throw new Error('Websocket instantiation failed! Address might be wrong.');
+				}
+			};
 
-	WebSocket.prototype.close = function() {
-		this.socket.close();
-	}
-	
-	WebSocket.prototype.getReadyState = function() {
-		this.socket.getReadyState();
-	}
-	///////////// Must be overloaded
-	WebSocket.prototype.onopen = function(){
-		throw new Error('onopen not implemented.');
-    };
-    
-    // alerts message pushed from server
-    WebSocket.prototype.onmessage = function(msg){
-    	throw new Error('onmessage not implemented.');
-    };
-    
-    // alerts message pushed from server
-    WebSocket.prototype.onerror = function(msg){
-    	throw new Error('onerror not implemented.');
-    };
-    
-    // alert close event
-    WebSocket.prototype.onclose = function(){
-        throw new Error('onclose not implemented.');
-    };
+			// storage to hold websocket object for later invokation of event methods
+			WebSocket.store = {};
+
+			// static event methods to call event methods on target websocket objects
+			WebSocket.onmessage = function (evt) {
+				WebSocket.store[evt._target]['onmessage'].call(global, evt);
+			}
+
+			WebSocket.onopen = function (evt) {
+				WebSocket.store[evt._target]['onopen'].call(global, evt);
+			}
+
+			WebSocket.onclose = function (evt) {
+				WebSocket.store[evt._target]['onclose'].call(global, evt);
+			}
+
+			WebSocket.onerror = function (evt) {
+				WebSocket.store[evt._target]['onerror'].call(global, evt);
+			}
+
+			// instance event methods
+			WebSocket.prototype.send = function(data) {
+				this.socket.send(data);
+			}
+
+			WebSocket.prototype.close = function() {
+				this.socket.close();
+			}
+
+			WebSocket.prototype.getReadyState = function() {
+				this.socket.getReadyState();
+			}
+			///////////// Must be overloaded
+			WebSocket.prototype.onopen = function(){
+				throw new Error('onopen not implemented.');
+			};
+
+			// alerts message pushed from server
+			WebSocket.prototype.onmessage = function(msg){
+				throw new Error('onmessage not implemented.');
+			};
+
+			// alerts message pushed from server
+			WebSocket.prototype.onerror = function(msg){
+				throw new Error('onerror not implemented.');
+			};
+
+			// alert close event
+			WebSocket.prototype.onclose = function(){
+				throw new Error('onclose not implemented.');
+			};
+		}
 })();
